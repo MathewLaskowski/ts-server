5a6d9e9f78f62ff91108a47f9d5b2eb066793463
mlaskowski <maciejlaskowsky@gmail.com>
2019-12-06 18:45:35 +0100

    add use decorator

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../appRouter';
 import { Methods } from './Methods';
+import { MetaDataKeys } from './MetaDataKeys';
 
 export function controller(routePrefix: string) {
   return function(target: Function) {
@@ -8,11 +9,12 @@ export function controller(routePrefix: string) {
 
     for(let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata('path', target.prototype, key);
-      const method: Methods = Reflect.getMetadata('method', target.prototype, key);
+      const path = Reflect.getMetadata(MetaDataKeys.path, target.prototype, key);
+      const method: Methods = Reflect.getMetadata(MetaDataKeys.method, target.prototype, key);
+      const middlewares = Reflect.getMetadata(MetaDataKeys.middleware, target, key) || [];
 
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler)
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler)
       }
     }
   }
