8699790126d125320a59caacea5d68214154c358
mlaskowski <maciejlaskowsky@gmail.com>
2019-12-08 00:04:41 +0100

    add RootController

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
 import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetaDataKeys } from './MetaDataKeys';
+import { RequestHandler } from 'express';
+
+interface RootHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
   return function (path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: RootHandlerDescriptor) {
       Reflect.defineMetadata(MetaDataKeys.path, path, target, key);
       Reflect.defineMetadata(MetaDataKeys.method, method, target, key);
     }
